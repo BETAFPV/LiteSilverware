c06f1ff827d4b1f45dfc994dd3b1ae7f4ce96af9
Xu-Zexiang <Tiga-Xu@users.noreply.github.com>
2019-04-18 15:30:31 +0800

    在头文件中声明dsm接收机绑定按键初始化函数与对频脉冲发送函数

--- Silverware/src/rx.h
+++ Silverware/src/rx.h
@@ -3,6 +3,11 @@
 void rx_init(void);
 void checkrx( void);
 #if defined(RX_DSMX_2048) || defined(RX_DSM2_1024)
+
+void lite_2S_BINDKEY_init(void);
+
+void lite_2S_rx_spektrum_bind(void);
+
 void rx_spektrum_bind(void);
 #endif
 
