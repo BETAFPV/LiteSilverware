4d671ad5c296be3cac75e8abd93bbacf07852d07
wenzhi <linkwenzhi@gmail.com>
2020-05-12 11:41:31 +0800

    delete unused file

--- Silverware/src/barometer.h
+++
@@ -1,9 +0,0 @@
-void barometer_init(void);
-int barometer_check(void);
-float read_pressure(void);
-
-
-void getEstimatedAltitude(void);
-
-void baro_update(void);
-
--- Silverware/src/bmp280_pressure.h
+++
@@ -1,5 +0,0 @@
-#include "bmp280.h"
-
-void delay_ms(uint32_t period_ms);
-int8_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
-int8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
